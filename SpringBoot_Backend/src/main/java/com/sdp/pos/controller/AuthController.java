77a2eb8b16c8e3f742ee45f9c934da6b17d9dc92
FirstOne <prosterz171@gmail.com>
2025-09-21 21:18:50 +0700

    feat: add verify user in auth

--- SpringBoot_Backend/src/main/java/com/sdp/pos/controller/AuthController.java
+++ SpringBoot_Backend/src/main/java/com/sdp/pos/controller/AuthController.java
@@ -6,22 +6,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sdp.pos.dto.auth.LoginRequestDTO;
 import com.sdp.pos.dto.auth.LoginResponseDTO;
 import com.sdp.pos.dto.user.UserRequestDTO;
+import com.sdp.pos.dto.user.UserResponseDTO;
 import com.sdp.pos.service.auth.contract.AuthService;
+import com.sdp.pos.service.user.contract.UserService;
 import com.sdp.pos.util.ApiResponse;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, UserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/register")
@@ -33,6 +39,12 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<LoginResponseDTO>> login(@Valid @RequestBody LoginRequestDTO requestDTO) {
         return ApiResponse.success("Login success", authService.login(requestDTO));
+    }
+
+    @GetMapping("/current-user")
+    public ResponseEntity<ApiResponse<UserResponseDTO>> currentUser(HttpServletRequest request) {
+        String userId = (String) request.getAttribute("user-id");
+        return ApiResponse.success("Verify success", userService.getById(userId));
     }
 
 }
